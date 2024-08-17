48d961340f33a98d33e14dda2e3250c71a1d866e
ikechan8370 <geyinchibuaa@gmail.com>
2024-08-17 12:49:24 +0800

    fix: meme api break change

--- karin-meme.js
+++ karin-meme.js
@@ -226,7 +226,7 @@ class memes extends plugin {
   }
 
   async randomMemes (e) {
-    let keys = Object.keys(infos).filter(key => infos[key].params.min_images === 1 && infos[key].params.min_texts === 0)
+    let keys = Object.keys(infos).filter(key => infos[key].params_type.min_images === 1 && infos[key].params_type.min_texts === 0)
     let index = _.random(0, keys.length - 1, false)
     console.log(keys, index)
     e.msg = infos[keys[index]].keywords[0]
@@ -260,7 +260,7 @@ class memes extends plugin {
     let formData = new FormData()
     let info = infos[targetCode]
     let fileLoc = []
-    if (info.params.max_images > 0) {
+    if (info.params_type.max_images > 0) {
       // 可以有图，来从回复、发送和头像找图
       let imgUrls = []
       if (e.reply_id) {
@@ -283,11 +283,11 @@ class memes extends plugin {
         // 如果都没有，用发送者的头像
         imgUrls = [await getAvatar(e)]
         // 如果再不够，加上机器人的头像
-        if (imgUrls.length < info.params.min_images) {
+        if (imgUrls.length < info.params_type.min_images) {
           imgUrls = [(await e.bot.getAvatarUrl())].concat(imgUrls)
         }
       }
-      if (imgUrls.length < info.params.min_images && imgUrls.indexOf(await getAvatar(e)) === -1) {
+      if (imgUrls.length < info.params_type.min_images && imgUrls.indexOf(await getAvatar(e)) === -1) {
         // 如果数量不够，补上发送者头像，且放到最前面
         let me = [await getAvatar(e)]
         let done = false
@@ -307,7 +307,7 @@ class memes extends plugin {
         }
         // imgUrls.push(`https://q1.qlogo.cn/g?b=qq&s=160&nk=${e.msg.sender.user_id}`)
       }
-      imgUrls = imgUrls.slice(0, Math.min(info.params.max_images, imgUrls.length))
+      imgUrls = imgUrls.slice(0, Math.min(info.params_type.max_images, imgUrls.length))
       for (let i = 0; i < imgUrls.length; i++) {
         let imgUrl = imgUrls[i]
         const imageResponse = await fetch(imgUrl)
@@ -321,26 +321,26 @@ class memes extends plugin {
         formData.append('images', new File([buffer], `avatar_${i}.jpg`, { type: 'image/jpeg' }))
       }
     }
-    if (text && info.params.max_texts === 0) {
+    if (text && info.params_type.max_texts === 0) {
       return false
     }
-    if (!text && info.params.min_texts > 0) {
+    if (!text && info.params_type.min_texts > 0) {
       if (e.at.length) {
         text = e.elements.filter(m => m.type === 'at')[0].name
       } else {
         text = e.sender.card || e.sender.nick
       }
     }
-    let texts = text.split('/', info.params.max_texts)
-    if (texts.length < info.params.min_texts) {
-      await e.reply(`字不够！要至少${info.params.min_texts}个用/隔开！`, { reply: true })
+    let texts = text.split('/', info.params_type.max_texts)
+    if (texts.length < info.params_type.min_texts) {
+      await e.reply(`字不够！要至少${info.params_type.min_texts}个用/隔开！`, { reply: true })
       return true
     }
     texts.forEach(t => {
       formData.append('texts', t)
     })
-    if (info.params.max_texts > 0 && formData.getAll('texts').length === 0) {
-      if (formData.getAll('texts').length < info.params.max_texts) {
+    if (info.params_type.max_texts > 0 && formData.getAll('texts').length === 0) {
+      if (formData.getAll('texts').length < info.params_type.max_texts) {
         if (e.at.length) {
           formData.append('texts', e.elements.filter(m => m.type === 'at')[0].name)
         } else {
@@ -485,8 +485,8 @@ function handleArgs (key, args, userInfos) {
 const detail = code => {
   let d = infos[code]
   let keywords = d.keywords.join('、')
-  let ins = `【代码】${d.key}\n【名称】${keywords}\n【最大图片数量】${d.params.max_images}\n【最小图片数量】${d.params.min_images}\n【最大文本数量】${d.params.max_texts}\n【最小文本数量】${d.params.min_texts}\n【默认文本】${d.params.default_texts.join('/')}\n`
-  if (d.params.args.length > 0) {
+  let ins = `【代码】${d.key}\n【名称】${keywords}\n【最大图片数量】${d.params_type.max_images}\n【最小图片数量】${d.params_type.min_images}\n【最大文本数量】${d.params_type.max_texts}\n【最小文本数量】${d.params_type.min_texts}\n【默认文本】${d.params_type.default_texts.join('/')}\n`
+  if (d.params_type.args.length > 0) {
     let supportArgs = ''
     switch (code) {
       case 'look_flat': {
